d752177884bc2c1f524f5990e6a492b3f6784fbf
kms3691h <kms3691h@nexivil.com>
2025-05-29 13:22:18 +0900

    `fix` modify default table style;

--- src/nodes/Excel/Worksheet/+addTable.js
+++ src/nodes/Excel/Worksheet/+addTable.js
@@ -25,16 +25,6 @@ export class addTable extends Pure {
       this.properties.startColumn,
       "startColumn"
     );
-    // this.addWidget(
-    //   "toggle",
-    //   "hasHeaders",
-    //   this.properties.hasHeaders,
-    //   "hasHeaders",
-    //   {
-    //     on: true,
-    //     off: false,
-    //   }
-    // );
   }
 
   async onExecute() {
@@ -42,20 +32,21 @@ export class addTable extends Pure {
     const data = this.getInputData(2) ?? {};
     const sr = this.getInputData(3) ?? this.properties.startRow;
     const sc = this.getInputData(4) ?? this.properties.startColumn;
-    const style = this.getInputData(4) ?? "TableStyleMedium1";
+    const style = this.getInputData(4) ?? "TableStyleMedium2";
     if (!ws) return;
     const { columns, rows } = data;
 
-    let rowCount = data.length;
-    let colCount = data[0]?.length ?? 0;
+    let rowCount = rows.length + 1;
+    let colCount = columns.length ?? 0;
     let range = ws.getRangeByIndexes(sr, sc, rowCount, colCount);
     let table = ws.tables.add(range, true);
-    table.getHeaderRowRange().values = columns;
+
+    table.getHeaderRowRange().values = [columns];
     table.getDataBodyRange().values = rows;
     table.set({
       style,
     });
-
+    await table.context.sync();
     this.setOutputData(1, table);
   }
 }
