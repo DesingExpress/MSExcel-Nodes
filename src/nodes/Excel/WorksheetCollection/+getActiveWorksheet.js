ebae84ed109b87ba627c6d9a6e7c7fde09be8322
kms3691h <kms3691h@nexivil.com>
2025-05-29 13:31:06 +0900

    `feat` add comment for context of ms-excel-office;

--- src/nodes/Excel/WorksheetCollection/+getActiveWorksheet.js
+++ src/nodes/Excel/WorksheetCollection/+getActiveWorksheet.js
@@ -7,6 +7,14 @@ export class getActiveWorksheet extends Pure {
 
   constructor() {
     super();
+    // In the Excel Office JavaScript API, context is an object that acts as the bridge between your JavaScript code and the Excel application.
+    // It provides access to the Excel workbook and allows you to queue up commands to interact with the workbook’s contents.
+    // 1. Access the workbook:
+    //    You can access the Excel workbook through context.workbook, which then lets you manipulate worksheets, ranges, tables, charts, etc.
+    // 2. Batch command execution:
+    //    Office.js uses a queued command model. You issue commands (like reading or writing to cells), and those commands are queued in the context until context.sync() is called.
+    // 3. Synchronization with Excel
+    //    context.sync() sends all queued commands to Excel for execution and updates your JavaScript objects with the latest values from Excel.
     this.addInput("context", "office::excel::context");
 
     this.addOutput("worksheet", "office::excel::worksheet");
