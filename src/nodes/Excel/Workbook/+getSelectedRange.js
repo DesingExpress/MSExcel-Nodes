e62d941cbce92261a570b60258616753ab4e1b8c
kms3691h <kms3691h@nexivil.com>
2025-07-08 16:14:09 +0900

    feat: simplify getSelectedRange output by removing unnecessary load and sync calls

--- src/nodes/Excel/Workbook/+getSelectedRange.js
+++ src/nodes/Excel/Workbook/+getSelectedRange.js
@@ -14,8 +14,6 @@ export class getSelectedRange extends Pure {
   async onExecute() {
     const _workbook = this.getInputData(1);
     const _range = _workbook.getSelectedRange();
-    _range.load("address");
-    await _range.context.sync();
-    this.setOutputData(1, _range.address);
+    this.setOutputData(1, _range);
   }
 }
